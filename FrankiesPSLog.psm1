fdb42590ae9581d0c6b0999ef4c21a2d1e0eef75
Frankie D RSVLT <57404682+Frankie-D-RSVLT@users.noreply.github.com>
2020-12-12 19:13:29 +0100

    Update FrankiesPSLog.psm1

--- FrankiesPSLog.psm1
+++ FrankiesPSLog.psm1
@@ -3,42 +3,64 @@
 function New-LogEntry {
     param (
         [parameter(ValueFromPipeline)]
-        [string]$Input,
-        [validateset('Info', 'Warn', 'Error', 'Debug')],
-        [string]$Level = "Info",
-        [Switch]$PrintToFile = $false
+        [string]$Message,
+        [validateset('Info', 'Warn', 'Error', 'Debug')]
+        [string]$Level = 'Info',
+        [bool]$PrintToOutput = $true,
+        [bool]$PrintToFile = $true
 
     )
 
-    $CurrentMessge = $LogObject.Data += @(
-        [pscustomobject]@{
-            'TimeStamp'   = Get-Date -UFormat "%Y-%m-%d %T (UTC %Z)"
-            'Level'       = $Level
-            'PrintToFile' = $PrintToFile
-            'Message'     = $Input
-        }
-    )
+    begin {
+
+    }
+
+    process {    
+        $global:LogObject.Data += $CurrentMessge = @(
+            [pscustomobject]@{
+                'TimeStamp'     = Get-Date -UFormat "%Y-%m-%d %T (UTC %Z)"
+                'Level'         = $Level
+                'Message'       = $Message
+                'PrintToOutput' = $PrintToOutput
+                'PrintToFile'   = $PrintToFile
+            }
+        )
+    }
+
+    end {
+        $CurrentMessge | Out-LogEntry
+    }
 
 }
 
 function Out-LogEntry {
     param (
-        [validateset('Info', 'Warn', 'Error', 'Debug')],
-        [string]$Level = "Info",
-        [switch]$PrintToFile = $true
+        [parameter(ValueFromPipeline)]
+        [pscustomobject]$Content
     )
 
-    $OutputMessage = ($CurrentMessge | Format-Table -HideTableHeaders | Out-String).Trim()
-
-    switch ($Level) {
-        'Info' { Write-Output $OutputMessage }
-        'Warn' { Write-Warning $OutputMessage }
-        'Error' { Write-Error $OutputMessage }
+    begin {
+        $LogFileCreated = 
     }
     
-    if ($PrintToFile) {
-        $OutputMessage | Out-File -Path $LogObject.Meta.FullFilePath -Encoding UTF8 -Append
-    }
+    process {}
+
+    end {}
+
+    $Content # | Format-Table -HideTableHeaders
+    $global:testcontent = $Content
+
+    # $OutputMessage = ($CurrentMessge | Format-Table -HideTableHeaders | Out-String).Trim()
+
+    # switch ($Level) {
+    #     'Info' { Write-Output $OutputMessage }
+    #     'Warn' { Write-Warning $OutputMessage }
+    #     'Error' { Write-Error $OutputMessage }
+    # }
+    
+    # if ($PrintToFile) {
+    #     $OutputMessage | Out-File -Path $global:LogObject.Meta.FullFilePath -Encoding UTF8 -Append
+    # }
 }
 
 function Initialize-Log {
@@ -46,78 +68,34 @@ function Initialize-Log {
         [string]$Name = 'UnnamedLog'
     )
     begin {
-        $LogObject = @()
-        $LogObject = [pscustomobject]@{
+        $global:LogObject = @()
+        $global:LogObject = [pscustomobject]@{
             Meta = @(
                 [pscustomobject]@{
                     FileName     = "$($Name)_$(Get-Date -Format FileDateTimeUniversal).log"
                     FullFilePath = $PSScriptRoot + '.\' + $FileName
+                    FileCreated = $false
                 }
             )
             Data = @(
-                [pscustomobject]@{
-                    'TimeStamp'   = Get-Date -UFormat "%Y-%m-%d %T (UTC %Z)"
-                    'Level'       = 'Info'
-                    'PrintToFile' = $true
-                    'Message'     = 'starting inizalizing'
-                }
-            )
-            
+                [pscustomobject]@{}
+            )            
         }
-        $LastLogMessage = $LogObject.Data | Select-Object -Last 1
+
+        $LastLogMessage = $global:LogObject.Data | Select-Object -Last 1
     }
 
     process {
-        $LogObject.Data += @(
-            [pscustomobject]@{
-                'TimeStamp'   = Get-Date -UFormat "%Y-%m-%d %T (UTC %Z)"
-                'Level'       = 'Info'
-                'PrintToFile' = $true
-                'Message'     = 'all date and time prints are in UTC, this includes the filename.'
-            }
-        )
-
-        $LogObject.Data += @(
-            [pscustomobject]@{
-                'TimeStamp'   = Get-Date -UFormat "%Y-%m-%d %T (UTC %Z)"
-                'Level'       = 'Info'
-                'PrintToFile' = $true
-                'Message'     = 'Computer: ' + $env:COMPUTERNAME
-            }
-        )
-
-        $LogObject.Data += @(
-            [pscustomobject]@{
-                'TimeStamp'   = Get-Date -UFormat "%Y-%m-%d %T (UTC %Z)"
-                'Level'       = 'Info'
-                'PrintToFile' = $true
-                'Message'     = 'User: ' + $env:USERNAME
-            }
-        )
-                
-        $LogObject.Data += @(
-            [pscustomobject]@{
-                'TimeStamp'   = Get-Date -UFormat "%Y-%m-%d %T (UTC %Z)"
-                'Level'       = 'Info'
-                'PrintToFile' = $true
-                'Message'     = 'FullFilePath: ' + $LogObject.Meta.FullFilePath
-            }
-        )
-        $LastLogMessage = $LogObject.Data | Select-Object -Last 1
-                
+        New-LogEntry -Level Info -PrintToOutput $true -PrintToFile $true -Message 'inizalizing starting'
+        New-LogEntry -Level Info -PrintToOutput $true -PrintToFile $true -Message 'all date and time prints are in UTC, this includes the filename.'
+        New-LogEntry -Level Info -PrintToOutput $true -PrintToFile $true -Message ('Computer: ' + $env:COMPUTERNAME)
+        New-LogEntry -Level Info -PrintToOutput $true -PrintToFile $true -Message ('User: ' + $env:USERNAME)
+        New-LogEntry -Level Info -PrintToOutput $true -PrintToFile $true -Message ('FullFilePath: ' + $global:LogObject.Meta.FullFilePath)
+        New-LogEntry -Level Info -PrintToOutput $true -PrintToFile $true -Message 'inizalizing finished'                
     }
 
     end {
-        $LogObject.Data += @(
-            [pscustomobject]@{
-                'TimeStamp'   = Get-Date -Format FileDateTimeUniversal
-                'Level'       = 'Info'
-                'PrintToFile' = $true
-                'Message'     = 'finished inizalizing'
-            }
-        )
-        $LastLogMessage = $LogObject.Data | Select-Object -Last 1
-
-        $LogObject.Data | Select-Object -ExcludeProperty PrintToFile | Out-File -Path $LogObject.FullFilePath
-    }
+        $global:LogObject.Data | Select-Object TimeStamp, Level, Message | Out-File -Path $global:LogObject.Meta.FullFilePath -Encoding utf8
+        
+        $global:LogObject.Meta = Test-Path $global:LogObject.Meta.FullFilePath
 }
